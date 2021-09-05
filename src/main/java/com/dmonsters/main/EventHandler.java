9ee56d851aa625d0e441ad693ea9561fd5e8cf8d
ACGaming <4818419+ACGaming@users.noreply.github.com>
2021-09-05 09:30:05 +0200

    Remove special Topielec spawning

--- src/main/java/com/dmonsters/main/EventHandler.java
+++ src/main/java/com/dmonsters/main/EventHandler.java
@@ -1,21 +1,14 @@
 package com.dmonsters.main;
 
-import java.util.Random;
-
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemSword;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent.SpecialSpawn;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -28,25 +21,6 @@ import com.dmonsters.network.PacketHandler;
 
 public class EventHandler
 {
-    @SubscribeEvent(priority = EventPriority.NORMAL)
-    public void onEntitySpawn(SpecialSpawn e)
-    {
-        if (ModConfig.topielecDisabled)
-        {
-            return;
-        }
-        Entity squid = e.getEntity();
-        if (squid instanceof EntitySquid)
-        {
-            Random rnd = new Random();
-            int rndInt = rnd.nextInt(100);
-            if (rndInt < ModConfig.topielecSpawnRate)
-            {
-                spawnEntity(squid, new EntityTopielec(squid.getEntityWorld()));
-            }
-        }
-    }
-
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public void onPlayerAttack(AttackEntityEvent e)
     {
@@ -81,17 +55,5 @@ public class EventHandler
             Item itemClass = player.getHeldItemMainhand().getItem();
             e.setCanceled(!(itemClass instanceof Harpoon));
         }
-    }
-
-    private void spawnEntity(Entity targetEntity, Entity entity)
-    {
-        World worldIn = targetEntity.getEntityWorld();
-        EntityLiving entityliving = (EntityLiving) entity;
-        entity.setLocationAndAngles(targetEntity.posX, targetEntity.posY, targetEntity.posZ, MathHelper.wrapDegrees(worldIn.rand.nextFloat() * 360.0F), 0.0F);
-        entityliving.rotationYawHead = entityliving.rotationYaw;
-        entityliving.renderYawOffset = entityliving.rotationYaw;
-        entityliving.onInitialSpawn(worldIn.getDifficultyForLocation(new BlockPos(entityliving)), null);
-        entityliving.playLivingSound();
-        worldIn.spawnEntity(entity);
     }
 }
